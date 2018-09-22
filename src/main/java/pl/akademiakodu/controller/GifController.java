cefbcd7c8dd32c16cd79406b345f715ffbc06fe6
KAMIL <kamil123_1992@o2.pl>
2018-09-22 13:12:21 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/controller/GifController.java
@@ -0,0 +1,16 @@
+package pl.akademiakodu.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+
+
+    @RequestMapping("/")
+    public String listGifs(){
+        return "home";
+    }
+}
