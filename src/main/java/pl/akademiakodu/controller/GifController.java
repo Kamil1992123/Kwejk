5e56e74c95b66a1cdc89511545dc64753bcc9f55
lukaszbirski <lukasz.birski@gmail.com>
2018-09-22 15:22:28 +0200

    new methode added

--- src/main/java/pl/akademiakodu/controller/GifController.java
+++ src/main/java/pl/akademiakodu/controller/GifController.java
@@ -2,8 +2,10 @@ package pl.akademiakodu.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.akademiakodu.model.Gif;
 
 @Controller
 public class GifController {
@@ -13,4 +15,12 @@ public class GifController {
     public String listGifs(){
         return "home";
     }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("android-exposion.gif", "michalos", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
+    }
 }
+
