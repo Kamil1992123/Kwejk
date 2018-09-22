2ede4f39121edfe2e999e0b9aeeea5b8f97114b4
KAMIL <kamil123_1992@o2.pl>
2018-09-22 15:26:21 +0200

    dsa

--- src/main/java/pl/akademiakodu/controller/GifController.java
+++ src/main/java/pl/akademiakodu/controller/GifController.java
@@ -2,8 +2,12 @@ package pl.akademiakodu.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.akademiakodu.model.Gif;
+
 
 @Controller
 public class GifController {
@@ -11,6 +15,12 @@ public class GifController {
 
     @RequestMapping("/")
     public String listGifs(){
+        return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif(true,"Android","asd");
         return "home";
     }
 }
